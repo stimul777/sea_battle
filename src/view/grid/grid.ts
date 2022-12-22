85a6bf0447ec70e83c6d0e08e8f07b04f3a2378b
devlyashov_yy <devlyashov_yy@magnit.ru>
2022-12-22 13:20:41 +0300

    [feature/project-structure] view двух сеток

--- src/view/grid/grid.ts
+++ src/view/grid/grid.ts
@@ -1,20 +1,22 @@
 //Построение игровой сетки
-export function onGrid(value: '.my-grid' | '.enemy-grid') {
+export function onGrid(value: 'my-grid' | 'enemy-grid') {
   const $wrapper = document.querySelector('.wrapper-grid');
   const $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-  // const $gridContainer =  document.createElement('div');
+
+  const container = document.createElement('div');
+  container.classList.add(value);
+  $wrapper?.append(container);
 
   for (let letter of $letters) {
     for (let i = 1; i <= $letters.length; i++) {
       const div = document.createElement('div');
-      $wrapper?.classList.add(value);
       div.classList.add(letter + i);
       div.innerHTML = letter + i;
       div.classList.add('square');
-      $wrapper?.appendChild(div);
+      container?.append(div);
     }
   }
 }
 
-onGrid('.my-grid');
-onGrid('.enemy-grid');
+onGrid('my-grid');
+onGrid('enemy-grid');
